43e4b0b67d77d9d2aecaefcb6b4af2cd06398d28
Oleg Shulga <oleg021086@gmail.com>
2021-01-03 22:53:56 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int remainder = balance + transfer;
+        System.out.println(remainder);
+    }
+
+}
